629b9581e63d80b5870143dfb31b418631e3e978
qapush <mihail.kapush@gmail.com>
2024-08-23 17:07:03 +0200

    setup module 2

--- app/not-found.jsx
+++
@@ -1,8 +0,0 @@
-function NotFound() {
-    return <main className="not-found">
-        <h1>Not found</h1>
-        <p>We cannot find the requested page</p>
-    </main>;
-}
-
-export default NotFound;
